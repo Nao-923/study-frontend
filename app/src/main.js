2c78393edc6611ac0d9b5f553c402d96b41682c9
DABU <daburutti@gmail.com>
2022-12-05 23:56:33 +0900

    OK

--- app/src/main.js
+++ app/src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
-import 'the-new-css-reset'
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "the-new-css-reset";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
--- app/src/views/Main.vue
+++ app/src/views/Main.vue
@@ -1,12 +1,121 @@
 <template>
-  <div class="main"></div>
+  <div class="top">
+    <el-menu default-active="1" class="el-menu-top" mode="horizontal">
+      <div class="container">
+        <el-row type="flex" class="" justify="space-between">
+          <div class="right">
+            Vitality
+            <div class="net">.net</div>
+          </div>
+          <div class="left">
+            <el-menu-item index="1"
+              ><span class="strong">S11065</span></el-menu-item
+            >
+          </div>
+        </el-row>
+      </div>
+    </el-menu>
+    <div class="line"></div>
+
+    <el-table
+      :data="
+        tableData.filter(
+          (data) =>
+            !search || data.name.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+      style="width: 100%"
+    >
+      <el-table-column label="Date" prop="date"> </el-table-column>
+      <el-table-column label="Name" prop="name"> </el-table-column>
+      <el-table-column align="right">
+        <template slot="header" slot-scope="">
+          <el-input v-model="search" size="mini" placeholder="Type to search" />
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+            >Edit</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            >Delete</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: "main",
+  name: "top",
   components: {},
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-02",
+          name: "John",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-04",
+          name: "Morgan",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-01",
+          name: "Jessy",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+      ],
+      search: "",
+    };
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.container {
+  max-width: 900px;
+  margin: auto;
+
+  .right {
+    height: 56px;
+    line-height: 56px;
+    font-weight: bold;
+    font-size: 25px;
+    position: relative;
+
+    .net {
+      font-size: 14px;
+      line-height: 14px;
+      position: absolute;
+      right: -25px;
+      bottom: 17px;
+      height: 14px;
+    }
+  }
+
+  .strong {
+    font-weight: bolder;
+  }
+}
+</style>
