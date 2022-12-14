2f73c43080f237d2d71e1a4dae55a617be2208d3
DABU <daburutti@gmail.com>
2022-12-14 01:14:52 +0000

    Fixぃんｔ

--- app/src/main.js
+++ app/src/main.js
@@ -5,12 +5,12 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "the-new-css-reset";
-import './index.css'
+import "./index.css";
 
-import VueGoodTablePlugin from 'vue-good-table';
+import VueGoodTablePlugin from "vue-good-table";
 
 // import the styles
-import 'vue-good-table/dist/vue-good-table.css'
+import "vue-good-table/dist/vue-good-table.css";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
